a36f0a8c3c820e8223c406c2b2c17df16afd7e68
Peem_rayu <91822674+Peerayukun@users.noreply.github.com>
2022-08-17 10:31:09 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const router = require('./routes/myRouter')
+const session = require('express-session')
+
+
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+
+app.use(express.urlencoded({extended:false}))
+app.use(session({secret:"mysession",resave:false,saveUninitialized:false}))
+app.use(router)
+// app.use(express.static(path.join(__dirname,'public')))
+
+app.listen(8080,()=>{
+    console.log('server start')
+})
