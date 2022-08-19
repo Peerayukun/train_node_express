605c6fdd2d17ca42eefcbf6fd60b5150c415bfd3
Peem_rayu <91822674+Peerayukun@users.noreply.github.com>
2022-08-19 09:23:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine','ejs')
 app.use(express.urlencoded({extended:false}))
 app.use(session({secret:"mysession",resave:false,saveUninitialized:false}))
 app.use(router)
-// app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.listen(8080,()=>{
     console.log('server start')
